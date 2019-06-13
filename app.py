df44569fa98119a34c149b302459cde6a27f7ae3
vFeng <z1914007838@outlook.com>
2019-06-13 15:00:52 +0800

    style

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 # -*- coding:utf-8 -*-
-import logging
-
-from jobs.proxy.proxy import Proxy
-from setting.config import PROXY_DB, PROXIES_CONFIG
-
 __author__ = 'zhoujifeng'
 __date__ = '2019/6/5 19:18'
 
+import logging
 from logging.config import dictConfig
 
+from jobs.proxy.proxy import Proxy
+from setting.config import PROXIES_CONFIG
 from utils.logger import LOGGING
+
 logger = logging.getLogger('default')
 
 if __name__ == '__main__':
     dictConfig(LOGGING)
 
-    p = Proxy(redis_db=PROXY_DB, **PROXIES_CONFIG)
+    p = Proxy(**PROXIES_CONFIG)
     p.server()
